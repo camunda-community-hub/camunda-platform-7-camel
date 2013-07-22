c82b3f9205a978ca0f3904a9a44690ea50a04a2b
Rafael Cordones <rafael@cordones.me>
2013-07-22 21:07:03 +0200

    Created submodule to contain common code to Sprgin and CDI implementations

---
+++ camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/common/CamundaBpmCamelConstants.java
@@ -0,0 +1,11 @@
+package org.camunda.bpm.camel.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rafa
+ * Date: 7/22/13
+ * Time: 9:02 PM
+ * @author Rafael Cordones
+ */
+public class CamundaBpmCamelConstants {
+}
