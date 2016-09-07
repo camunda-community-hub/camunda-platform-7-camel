43694dc3307b729aa800d6652a9082f468c1466c
Stephan Pelikan <stephan.pelikan@wdw-elab.de>
2016-09-07 15:41:19 +0200

    Fixes

--- camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/component/externaltasks/TaskProcessor.java
+++ camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/component/externaltasks/TaskProcessor.java
@@ -1,6 +1,7 @@
 package org.camunda.bpm.camel.component.externaltasks;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
@@ -15,6 +16,8 @@ import org.camunda.bpm.engine.externaltask.LockedExternalTask;
 
 public class TaskProcessor implements Processor {
 
+    private static final Logger logger = Logger.getLogger(TaskProcessor.class.getCanonicalName());
+
     private final CamundaBpmEndpoint camundaEndpoint;
 
     // parameters
@@ -25,11 +28,11 @@ public class TaskProcessor implements Processor {
     private final boolean onCompletion;
     private final String topic;
     private final String workerId;
-    
-    public TaskProcessor(final CamundaBpmEndpoint endpoint, final String topic,
-    		final int retries, final long retryTimeout, final long[] retryTimeouts,
-    		final boolean completeTask, final boolean onCompletion, final String workerId) {
-		
+
+    public TaskProcessor(final CamundaBpmEndpoint endpoint, final String topic, final int retries,
+            final long retryTimeout, final long[] retryTimeouts, final boolean completeTask, final boolean onCompletion,
+            final String workerId) {
+
         this.camundaEndpoint = endpoint;
         this.retries = retries;
         this.retryTimeout = retryTimeout;
@@ -37,45 +40,45 @@ public class TaskProcessor implements Processor {
         this.completeTask = completeTask;
         this.onCompletion = onCompletion;
         this.workerId = workerId;
-    	this.topic = topic;
-    	
-	}
-    
+        this.topic = topic;
+
+    }
+
     private ExternalTaskService getExternalTaskService() {
 
         return camundaEndpoint.getProcessEngine().getExternalTaskService();
 
     }
 
-	@Override
-	public void process(final Exchange exchange) {
-		
-	    if (!onCompletion) {
-	        
-	        internalProcessing(exchange);
-	        
-	    } else {
-	        
-		final TaskProcessor taskProcessor = this;
-        exchange.addOnCompletion(new Synchronization() {
+    @Override
+    public void process(final Exchange exchange) {
 
-            @Override
-            public void onFailure(final Exchange exchange) {
-            	taskProcessor.internalProcessing(exchange);
-            }
+        if (!onCompletion) {
 
-            @Override
-            public void onComplete(final Exchange exchange) {
-            	taskProcessor.internalProcessing(exchange);
-            }
+            internalProcessing(exchange);
 
-        });
-		
+        } else {
+
+            final TaskProcessor taskProcessor = this;
+            exchange.addOnCompletion(new Synchronization() {
+
+                @Override
+                public void onFailure(final Exchange exchange) {
+                    taskProcessor.internalProcessing(exchange);
+                }
+
+                @Override
+                public void onComplete(final Exchange exchange) {
+                    taskProcessor.internalProcessing(exchange);
+                }
+
+            });
+
         }
 
-	}
-    
-	@SuppressWarnings("unchecked")
+    }
+
+    @SuppressWarnings("unchecked")
     void internalProcessing(final Exchange exchange) {
 
         final Message in = exchange.getIn();
@@ -84,45 +87,56 @@ public class TaskProcessor implements Processor {
         }
 
         final ExternalTaskService externalTaskService = getExternalTaskService();
-        
+
         final LockedExternalTask lockedTask = in.getHeader(CamundaBpmPollExternalTasksEndpointImpl.EXCHANGE_HEADER_TASK,
                 LockedExternalTask.class);
         final String lockedTaskId = in.getHeader(CamundaBpmPollExternalTasksEndpointImpl.EXCHANGE_HEADER_TASKID,
                 String.class);
-        
+
         final String taskId;
         if (lockedTask != null) {
-        	taskId = lockedTask.getId();
-        }
-        else if (lockedTaskId != null) {
-        	taskId = lockedTaskId;
-        }
-        else {
+            taskId = lockedTask.getId();
+        } else if (lockedTaskId != null) {
+            taskId = lockedTaskId;
+        } else {
             throw new RuntimeCamelException("Unexpected exchange: in-header '"
                     + CamundaBpmPollExternalTasksEndpointImpl.EXCHANGE_HEADER_TASK + "' and '"
                     + CamundaBpmPollExternalTasksEndpointImpl.EXCHANGE_HEADER_TASKID + "' is null!");
         }
-        
-        final ExternalTask task = getExternalTaskService().createExternalTaskQuery().externalTaskId(taskId).singleResult();
-        if ((task.getWorkerId() != null)
-        		&& (workerId != null)
-        		&& !task.getWorkerId().equals(workerId)) {
-            throw new RuntimeCamelException("Unexpected exchange: the external task '"
-                    + taskId + "' is locked for worker '"
-                    + task.getWorkerId() + "' which differs from the configured worker '"
-                    + workerId + "!");
+
+        final ExternalTask task = getExternalTaskService().createExternalTaskQuery().externalTaskId(
+                taskId).singleResult();
+        if (task != null) {
+            if ((task.getWorkerId() != null) && (workerId != null) && !task.getWorkerId().equals(workerId)) {
+                throw new RuntimeCamelException(
+                        "Unexpected exchange: the external task '" + taskId + "' is locked for worker '"
+                                + task.getWorkerId() + "' which differs from the configured worker '" + workerId + "!");
+            }
+            if ((task.getTopicName() != null) && (topic != null) && !task.getTopicName().equals(topic)) {
+                throw new RuntimeCamelException(
+                        "Unexpected exchange: the external task '" + taskId + "' is from topic '" + task.getWorkerId()
+                                + "' which differs from the configured topic '" + topic + "!");
+            }
         }
-        if ((task.getTopicName() != null)
-        		&& (topic != null)
-        		&& !task.getTopicName().equals(topic)) {
-            throw new RuntimeCamelException("Unexpected exchange: the external task '"
-                    + taskId + "' is from topic '"
-                    + task.getWorkerId() + "' which differs from the configured topic '"
-                    + topic + "!");
+
+        // 'process-externalTask' at the end of a route does not have any output
+        // - only input which was any proceeder's output.
+        final Message out;
+        if (exchange.hasOut()) {
+            out = exchange.getOut();
+        } else {
+            out = in;
         }
-        		
+
         // failure
         if (exchange.isFailed()) {
+
+            if (task == null) {
+                logger.warning(
+                        "Processing failed but the task seems to be already processed - will do nothing! Camnda external task id: '"
+                                + taskId + "'");
+                return;
+            }
 
             final int retries;
             if (task.getRetries() == null) {
@@ -142,9 +156,16 @@ public class TaskProcessor implements Processor {
 
         } else
         // bpmn error
-        if ((in != null) && (in.getBody() != null) && (in.getBody() instanceof String)) {
+        if ((out != null) && (out.getBody() != null) && (out.getBody() instanceof String)) {
 
-            final String errorCode = in.getBody(String.class);
+            final String errorCode = out.getBody(String.class);
+
+            if (task == null) {
+                logger.warning("Should complete the external task with BPM error '" + errorCode
+                        + "' but the task seems to be already processed - will do nothing! Camnda external task id: '"
+                        + taskId + "'");
+                return;
+            }
 
             externalTaskService.handleBpmnError(task.getId(), task.getWorkerId(), errorCode);
 
@@ -152,9 +173,15 @@ public class TaskProcessor implements Processor {
         // success
         if (completeTask) {
 
+            if (task == null) {
+                logger.warning("Should complete the external task but the task seems to be "
+                        + "already processed - will do nothing! Camnda external task id: '" + taskId + "'");
+                return;
+            }
+
             final Map<String, Object> variablesToBeSet;
-            if ((in != null) && (in.getBody() != null) && (in.getBody() instanceof Map)) {
-                variablesToBeSet = in.getBody(Map.class);
+            if ((out != null) && (out.getBody() != null) && (out.getBody() instanceof Map)) {
+                variablesToBeSet = out.getBody(Map.class);
             } else {
                 variablesToBeSet = null;
             }
@@ -166,8 +193,8 @@ public class TaskProcessor implements Processor {
             }
 
         }
-		
-	}
+
+    }
 
     private long calculateTimeout(final int retries) {
 
@@ -181,5 +208,5 @@ public class TaskProcessor implements Processor {
         }
 
     }
-	
+
 }
