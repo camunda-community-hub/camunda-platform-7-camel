1c30380d251551ab1ba4d9831c02e7f7745adf1d
Jan Galinski <jan.galinski@holisticon.de>
2022-02-03 00:47:00 +0100

    add license header

--- camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/common/UriUtils.java
+++ camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/common/UriUtils.java
@@ -1,3 +1,15 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.camunda.bpm.camel.common;
 
 public class UriUtils {
@@ -47,14 +59,14 @@ public class UriUtils {
 
     }
 
-    public static enum UriType {
+    public enum UriType {
 
         StartProcess("start"), SendSignal("signal"), SendMessage("message"),
         		PollExternalTasks("poll-externalTasks"), ProcessExternalTask("async-externalTask");
 
         private String identifier;
 
-        private UriType(final String identifier) {
+        UriType(final String identifier) {
             this.identifier = identifier;
         }
 
--- camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/component/CamundaBpmEndpointDefaultImpl.java
+++ camunda-bpm-camel-common/src/main/java/org/camunda/bpm/camel/component/CamundaBpmEndpointDefaultImpl.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.camunda.bpm.camel.component;
 
 import java.util.Map;
